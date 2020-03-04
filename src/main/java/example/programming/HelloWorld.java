536036dedefbb66bdbaf1651017a96ecd549449f
Andi365 <andi9andi10>
2020-03-04 14:55:48 +0100

    initial commit

---
+++ src/main/java/example/programming/HelloWorld.java
@@ -0,0 +1,11 @@
+package example.programming;
+
+import ev3dev.actuators.LCD;
+
+public class HelloWorld {
+
+    public static void main(final String[] args) {
+        //LCD.getInstance().drawChars('Helloworld',0,10,1,0,0);
+    }
+
+}
